07cf244af66265bdb2b77c4cc3945d702cde4509
ivan.gonzalez <ivan.gonzalez@redegal.com>
2018-11-10 22:06:50 +0100

    Save localStorage mode view product

--- _dev/js/components/view-mode-products.js
+++ _dev/js/components/view-mode-products.js
@@ -1,21 +1,42 @@
 import $ from 'jquery';
-import prestashop from 'prestashop';
 
 export default class ViewModeProducts {
     init(){
         this.viewModeProducts();
+        this.domInitViewMode();
     }
-    //TO-DO
-    //Save on localStorage a Theme Name Mode View
+
     viewModeProducts() {
         $('.js-view-product-mode').on(
             'click',
             (event) => {
                 $('#js-product-list').toggleClass('js-product-mode-list');
                 $('.js-view-product-mode').children('i').first().text(function(i, v){
+                    saveViewModeProduct(v);
                     return v === 'view_list' ? 'view_module' : 'view_list';
-                })
+                });
             }
         ); 
+    }
+    domInitViewMode() {
+        if(typeof(Storage) !== "undefined") {
+            if (localStorage.view_mode_product) {
+                var selected_view_mode_product = localStorage.getItem("view_mode_product");
+                if (selected_view_mode_product === 'view_list') {
+                    $('#js-product-list').toggleClass('js-product-mode-list');
+                    $('.js-view-product-mode').children('i').first().text(function(i, v){
+                        return v === 'view_list' ? 'view_module' : 'view_list';
+                    });
+                }
+            }
+        }
+    }
+}
+
+function saveViewModeProduct(data) {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.view_mode_product) {
+            localStorage.view_mode_product = data;
+        }
     }
 }
